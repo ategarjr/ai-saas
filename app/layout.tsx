34c414594f2cd20cde4e4fadcedf2fe1a5fa5eaf
ategarjr <ategarjr@gmail.com>
2025-03-16 20:49:58 +0700

    init: initialize project & setup color and font

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Source_Sans_3 as FontSourceSans } from "next/font/google";
+import "./globals.css";
+
+const fontSourceSans = FontSourceSans({
+  variable: "--font-source-sans",
+  subsets: ["latin"],
+  weight: ['200', '400', '500', '600', '700', '800', '900'],
+});
+
+export const metadata: Metadata = {
+  title: "Sommaire - AI-Powered PDF Summarization",
+  description:
+    "Save hours of reading time. Transform lengthy PDFs into clear, accurate summaries in seconds with our advanced AI technology.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${fontSourceSans.variable} font-sans antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
