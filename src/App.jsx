0d3f236020e41c8b9e93ee2d05cff02c335e73d7
ategarjr <ategarjr@gmail.com>
2025-04-17 09:30:36 +0700

    initialize: set up assets - image and logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
+
 import './App.css'
 
-function App() {
+const App = () => {
+  
+
   return (
-    <>
       <main>
-        <header>
-          <h1>Hello</h1>
-        </header>
+        <div className='pattern'></div>
+        <div className='wrapper'>
+          <header>
+            <img src="./hero.png" alt="Herro Banner" />
+            <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without The Hassle</h1>
+          </header>
+        </div>
       </main>
-    </>
   )
 }
 
